b6d8a5ce770c471125dfdbb4580cddc588b79429
Nataliia Liubynets <natalialiubynets@gmail.com>
2023-10-04 20:25:13 +0300

    deleted comments

--- src/index.js
+++ src/index.js
@@ -5,13 +5,8 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerJsdoc = require('./swaggerConfig');
 const routes = require('./routes/routes');
 
-
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerJsdoc));
 app.use(routes);
-
-// app.get('/health-check', (req, res) => {
-// 	res.json({server: `Server is running on port ${PORT}`});
-// });
 
 app.listen(PORT, () => {
 	console.log(`Server is listening on port ${PORT}`);
