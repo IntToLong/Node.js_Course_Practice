973c646c9766c7f466ff65c0345b492ad7e67164
Nataliia Liubynets <natalialiubynets@gmail.com>
2023-10-30 00:29:23 +0200

    added Swagger documentation for Genres API

--- src/routes/genre.route.ts
+++ src/routes/genre.route.ts
@@ -2,6 +2,63 @@ import { NextFunction, Request, Response, Router } from 'express';
 import Genre from '../models/genre.model';
 
 const router: Router = Router();
+/**
+ * @swagger
+ * components: 
+ *    schemas: 
+ *      Genre:
+ *       type: object
+ *       properties:
+ *         name:
+ *            type: string
+ *       example:
+ *         name: "Sci-Fi"
+ */
+
+/**
+ * @swagger
+ * /genres:
+ *   post:
+ *     tags:
+ *       - Genres
+ *     summary: Create a new genre
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *               type: object
+ *               properties:
+ *                  name:
+ *                     type: string
+ *               example:
+ *                      name: "Sci-Fi"
+ *     responses:
+ *       '201':
+ *         description: Created
+ *       '400':
+ *         description: Invalid request.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *               example:
+ *                 error: "Invalid request."
+ *       '500':
+ *         description: Internal server error.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *               example:
+ *                 error: "Internal server error."
+ */
 
 // Create a new genre
 router.post('/genres', async (req: Request, res: Response, next: NextFunction) => {
@@ -14,7 +71,39 @@ router.post('/genres', async (req: Request, res: Response, next: NextFunction) =
     next(error);
   }
 });
-
+/**
+ * @swagger
+ * /genres:
+ *   get:
+ *     tags:
+ *       - Genres
+ *     summary: Get all genres
+ *     responses:
+ *       '200':
+ *         description: Successful response.
+ *       '400':
+ *         description: Invalid request.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *               example:
+ *                 error: "Invalid request."
+ *       '500':
+ *         description: Internal server error.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *               example:
+ *                 error: "Internal server error."
+ */
 // Get all genres
 router.get('/genres', async (req: Request, res: Response, next: NextFunction) => {
   try {
@@ -24,6 +113,59 @@ router.get('/genres', async (req: Request, res: Response, next: NextFunction) =>
     next(error);
   }
 });
+
+
+/**
+ * @swagger
+ * /genres/{id}:
+ *   put:
+ *     tags:
+ *       - Genres
+ *     summary: Update a genre by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: The ID of the genre to update.
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *             example:
+ *               name: "Updated Genre Name"
+ *     responses:
+ *       '200':
+ *         description: Successful response.
+ *       '400':
+ *         description: Invalid request.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *               example:
+ *                 error: "Invalid request."
+ *       '500':
+ *         description: Internal server error.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *               example:
+ *                 error: "Internal server error."
+ */
 
 // Update a genre
 router.put('/genres/:id', async (req: Request, res: Response, next: NextFunction) => {
@@ -37,6 +179,46 @@ router.put('/genres/:id', async (req: Request, res: Response, next: NextFunction
   }
 });
 
+/**
+ * @swagger
+ * /genres/{id}:
+ *   delete:
+ *     tags:
+ *       - Genres
+ *     summary: Delete a genre by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: The ID of the genre to delete.
+ *         schema:
+ *           type: string
+ *     responses:
+ *       '200':
+ *         description: Successful response.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *               example:
+ *                 message: "Genre deleted successfully."
+ *       '400':
+ *         description: Invalid request.
+ *       '500':
+ *         description: Internal server error.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *               example:
+ *                 error: "Internal server error."
+ */
 // Delete a genre
 router.delete('/genres/:id', async (req: Request, res: Response, next: NextFunction) => {
   const { id } = req.params;
