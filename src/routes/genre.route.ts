89a0ad957a7f6c6cc17a5dbed6f5271a4d272de1
Nataliia Liubynets <natalialiubynets@gmail.com>
2023-10-29 21:50:42 +0200

    created an API for "Movies"

--- src/routes/genre.route.ts
+++ src/routes/genre.route.ts
@@ -1,58 +1,50 @@
-import { Request, Response, Router } from 'express';
+import { NextFunction, Request, Response, Router } from 'express';
 import Genre from '../models/genre.model';
 
 const router: Router = Router();
 
-
 // Create a new genre
-router.post('/genres', async (req: Request, res: Response) => {
+router.post('/genres', async (req: Request, res: Response, next: NextFunction) => {
   const { name } = req.body;
-
   try {
     const genre = new Genre({ name });
     await genre.save();
     res.send(genre);
   } catch (error: unknown) {
-    console.error(error);
-    res.status(500).send(error);
+    next(error);
   }
 });
 
 // Get all genres
-router.get('/genres', async (req: Request, res: Response) => {
+router.get('/genres', async (req: Request, res: Response, next: NextFunction) => {
   try {
     const genres = await Genre.find({});
     res.send(genres);
   } catch (error: unknown) {
-    console.error(error);
-    res.status(500).send(error);
+    next(error);
   }
 });
 
 // Update a genre
-router.put('/genres/:id', async (req: Request, res: Response) => {
+router.put('/genres/:id', async (req: Request, res: Response, next: NextFunction) => {
   const { id } = req.params;
   const { name } = req.body;
-
   try {
     const genre = await Genre.findByIdAndUpdate(id, { name }, { new: true });
     res.send(genre);
   } catch (error: unknown) {
-    console.error(error);
-    res.status(500).send(error);
+    next(error);
   }
 });
 
 // Delete a genre
-router.delete('/genres/:id', async (req: Request, res: Response) => {
+router.delete('/genres/:id', async (req: Request, res: Response, next: NextFunction) => {
   const { id } = req.params;
-
   try {
     const genre = await Genre.findByIdAndDelete(id);
     res.send(genre);
   } catch (error: unknown) {
-    console.error(error);
-    res.status(500).send(error);
+    next(error);
   }
 });
 
--- src/routes/movie.route.ts
+++ src/routes/movie.route.ts
@@ -1,4 +1,6 @@
-import { Request, Response, Router } from 'express';
+import { NextFunction, Request, Response, Router } from 'express';
+
+import Movie from '../models/movie.model';
 
 const router: Router = Router();
 
@@ -69,6 +71,64 @@ router.post('/health-set', (req: Request, res: Response): void => {
     res.status(201).json({ status: 'New Server status!' });
   } else {
     res.status(400).send('Bad request. Server status should be string type.');
+  }
+});
+
+// Create a new movie
+router.post('/movies', async (req: Request, res: Response, next: NextFunction) => {
+  const { title, description, releaseDate, genre } = req.body;
+
+  try {
+    const movie = new Movie({ title, description, releaseDate, genre });
+    await movie.save();
+    res.send(movie);
+  } catch (error: unknown) {
+    next(error);
+  }
+});
+
+// Get all movies
+router.get('/movies', async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const movies = await Movie.find({});
+    res.send(movies);
+  } catch (error: unknown) {
+    next(error);
+  }
+});
+
+// Update a movie
+router.put('/movies/:id', async (req: Request, res: Response, next: NextFunction) => {
+  const { id } = req.params;
+  const { title, description, releaseDate, genre } = req.body;
+
+  try {
+    const movie = await Movie.findByIdAndUpdate(id, { title, description, releaseDate, genre }, { new: true });
+    res.send(movie);
+  } catch (error: unknown) {
+    next(error);
+  }
+});
+
+// Delete a movie
+router.delete('/movies/:id', async (req: Request, res: Response, next: NextFunction) => {
+  const { id } = req.params;
+  try {
+    const movie = await Movie.findByIdAndDelete(id);
+    res.send(movie);
+  } catch (error: unknown) {
+    next(error);
+  }
+});
+
+//searching for Movies by Genre
+router.get('/movies/genre/:genreName', async (req: Request, res: Response, next: NextFunction) => {
+  const { genreName } = req.params;
+  try {
+    const movies = await Movie.find({ genre: { $in: new RegExp(genreName, 'i')} });
+    res.send(movies);
+  } catch (error: unknown) {
+    next(error);
   }
 });
 
