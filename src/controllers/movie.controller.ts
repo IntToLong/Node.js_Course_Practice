42c8fc2d4f22ff5b0ec5d179633b718dbcc91c45
Nataliia Liubynets <natalialiubynets@gmail.com>
2023-11-05 15:59:25 +0200

    added controllers

---
+++ src/controllers/movie.controller.ts
@@ -0,0 +1,62 @@
+import { NextFunction, Request, Response } from 'express';
+import { movieSchema } from '../middleware/data_validation/schemas';
+import Movie from '../models/movie.model';
+
+export const createMovie = async (req: Request, res: Response, next: NextFunction) => {
+  const { title, description, releaseDate, genre } = req.body;
+  const { error } = movieSchema.validate(req.body);
+  if (error) {
+    return res.status(400).json({ error: error.details[0].message });
+  } else {
+    try {
+      const movie = new Movie({ title, description, releaseDate, genre });
+      await movie.save();
+      res.send(movie);
+    } catch (error: unknown) {
+      next(error);
+    }
+  }
+};
+
+export const getAllMovies = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const movies = await Movie.find({});
+    res.send(movies);
+  } catch (error: unknown) {
+    next(error);
+  }
+};
+
+export const updateMovie = async (req: Request, res: Response, next: NextFunction) => {
+  const { id } = req.params;
+  const { title, description, releaseDate, genre } = req.body;
+
+  try {
+    const movie = await Movie.findByIdAndUpdate(id, { title, description, releaseDate, genre }, { new: true });
+    res.send(movie);
+  } catch (error: unknown) {
+    next(error);
+  }
+};
+
+export const deleteMovie = async (req: Request, res: Response, next: NextFunction) => {
+  const { id } = req.params;
+  try {
+    const movie = await Movie.findByIdAndDelete(id);
+    res.send(movie);
+  } catch (error: unknown) {
+    next(error);
+  }
+}
+
+export const searchMoviesByGenre = async (req: Request, res: Response, next: NextFunction) => {
+  const { genreName } = req.params;
+  try {
+    const movies = await Movie.find({ genre: { $in: new RegExp(genreName, 'i') } });
+    res.send(movies);
+  } catch (error: unknown) {
+    next(error);
+  }
+};
+
+export * as movieController from './movie.controller';
